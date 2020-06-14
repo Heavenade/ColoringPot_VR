30c752fa22aad60018568288b094129ca6fc0e26
KimKyuRi <heavenade28@gmail.com>
2020-06-14 18:06:59 +0900

    ADD: 사운드 리소스 및 AudioManager 스크립트 추가
    1. 이후 각 씬에서의 연결 필요

---
+++ Assets/Script/Audio/BGMManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMManager : MonoBehaviour
+{
+    static public BGMManager instance;
+
+    public AudioClip[] clips; // BGM 리스트
+    private AudioSource source;
+
+    private WaitForSeconds waitTime = new WaitForSeconds(0.01f);
+
+    private float BGMVolume; //BGM 볼륨
+
+    private int BGMnowplaying; //현재 재생중인 BGM
+
+    /*씬 이름들*/
+    public const string titlescene = "Title_Tmp";//이런 형식들
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        source = this.GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+        AutoSelectBGM(SceneManager.GetActiveScene(), LoadSceneMode.Single);//BGM 셀렉
+        PlayBGM(BGMnowplaying);//BGM 재생
+
+        SceneManager.sceneLoaded += AutoSelectBGM;
+    }
+
+    /*씬 변경 시마다 BGM 적용*/
+    public void AutoSelectBGM(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        int preBGMnowPlaying = BGMnowplaying;
+        int nextBGMnowPlaying = preBGMnowPlaying;
+
+        string curscene = scene.name;
+
+        /*씬 추가 될 경우 BGM control*/
+        if (curscene == titlescene)//타이틀 씬 (메인)
+        {
+            nextBGMnowPlaying = 0;
+        }
+
+
+        /*BGM 변경 시*/
+        if (preBGMnowPlaying != nextBGMnowPlaying)
+        {
+            BGMnowplaying = nextBGMnowPlaying;
+            FadeOutBGM();
+            //StopBGM();
+            PlayBGM(BGMnowplaying);
+            FadeInBGM();
+        }
+    }
+
+    /*BGM 재생*/
+    public void PlayBGM(int _playMusicTrack)
+    {
+        source.volume = BGMVolume;
+        source.clip = clips[_playMusicTrack];
+        source.Play();
+    }
+
+    /*볼륨 조정*/
+    public void SetBGMVolume(float _volume)
+    {
+        BGMVolume = _volume;
+        source.volume = BGMVolume;
+    }
+
+    /*음악 일시 정지*/
+    public void PauseBGM()
+    {
+        source.Pause();
+    }
+
+    /*음악 일시 정지 해제*/
+    public void UnpauseBGM()
+    {
+        source.UnPause();
+    }
+
+    /*음악 재생 중단*/
+    public void StopBGM()
+    {
+        source.Stop();
+    }
+
+    /*음악 페이드 아웃*/
+    public void FadeOutBGM()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeOutBGMCoroutine());
+    }
+
+    IEnumerator FadeOutBGMCoroutine()
+    {
+        for (float i = BGMVolume; i >= 0f; i -= 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
+
+    /*음악 페이드 인*/
+    public void FadeInBGM()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FadeInBGMCoroutine());
+    }
+    IEnumerator FadeInBGMCoroutine()
+    {
+        for (float i = 0f; i <= BGMVolume; i += 0.01f)
+        {
+            source.volume = i;
+            yield return waitTime;
+        }
+    }
+}
---
+++ Assets/Script/Audio/EffectManager.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+
+public class Sound
+{
+    public string name; //이펙트 사운드의 이름.
+
+    public AudioClip clip; //이펙트 사운드 파일
+    private AudioSource source; //이펙트 사운드 플레이어
+
+
+    private bool loop;//루프 여부
+
+    /*이펙트 사운드 소스 지정*/
+    public void SetSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+        source.loop = loop;
+        source.volume = EffectManager.instance.EffectVolume;
+    }
+
+    /*받아온 이펙트 사운드 볼륨을 실제로 적용*/
+    public void SetVolume(float _volume)
+    {
+        source.volume = _volume;
+    }
+
+    /*이펙트 사운드 재생*/
+    public void Play()
+    {
+        source.Play();
+    }
+    /*이펙트 사운드 중단*/
+    public void Stop()
+    {
+        source.Stop();
+    }
+    /*이펙트 사운드 루프*/
+    public void SetLoop()
+    {
+        source.loop = true;
+    }
+    /*이펙트 사운드 루프 중단*/
+    public void SetLoopCancel()
+    {
+        source.loop = false;
+    }
+}
+
+public class EffectManager : MonoBehaviour
+{
+    static public EffectManager instance;
+
+    [SerializeField]
+    public Sound[] sounds;//이펙트 사운드 리스트
+
+    public float EffectVolume;//이펙트 사운드 볼륨
+
+    //public bool EMStartEnd;
+
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject soundObject = new GameObject(sounds[i].name);
+            sounds[i].SetSource(soundObject.AddComponent<AudioSource>());
+            soundObject.transform.SetParent(this.transform);
+        }
+    }
+
+    void Start()
+    {
+        EffectVolume = 1f; //초기 볼륨
+
+        SetLoopInit();//루프 초기값
+    }
+
+    /*사운드 이름을 참조해 개별 이펙트 사운드 재생 or 중단 */
+    public void Play(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (_name == sounds[i].name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+    }
+    public void Stop(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (_name == sounds[i].name)
+            {
+                sounds[i].Stop();
+                return;
+            }
+        }
+    }
+
+    /*사운드 이름을 참조해 개별 이펙트 사운드 루프 재생 or 중단*/
+    public void SetLoop(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (_name == sounds[i].name)
+            {
+                sounds[i].SetLoop();
+                return;
+            }
+        }
+    }
+    public void SetLoopCancel(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (_name == sounds[i].name)
+            {
+                sounds[i].SetLoopCancel();
+                return;
+            }
+        }
+    }
+
+    /*이펙트 사운드별 루프 재생 초기값 설정*/
+    public void SetLoopInit()
+    {
+        //루프가 필요한 이펙트가 있다면 
+    }
+
+    /*모든 이펙트 사운드 볼륨 설정*/
+    public void SetEffectVolume(float _volume)
+    {
+        EffectVolume = _volume;
+
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            sounds[i].SetVolume(EffectVolume);
+        }
+    }
+}
+
