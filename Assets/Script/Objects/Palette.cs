fca8d76ef49d3e4dfbac8b77b81074afaf9cd94e
seongbuming <sbum.seo@gmail.com>
2020-06-16 18:27:25 +0900

    Update: 채색(팔레트&붓 상호작용)

---
+++ Assets/Script/Objects/Palette.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Palette : MonoBehaviour
+{
+    public Transform brush;
+    public PotteryColoring pottery;
+    public int colorNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SphereCollider collider = this.GetComponent<SphereCollider>();
+        float distance = Vector3.Distance(this.transform.position + collider.center, brush.transform.position);
+        if (distance < collider.radius / 4)
+        {
+            pottery.colorNum = colorNum;
+        }
+    }
+}
